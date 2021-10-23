a000dad5fca42e49fa9b42bb130d3163a4e3e80e
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-10-23 20:32:06 +0530

    feat - Creating server and adding the helmet express and morgan packages
    also added the usage of routes and connected the MongoDB

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+const userRoutes = require("./routes/users");
+const AuthRoutes = require("./routes/auth");
+
+
+//to use dotenv 
+dotenv.config();
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
+    
+    console.log("Connected to Mongodb")
+})
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+// app.get("/",(req,res)=>{
+
+//     res.send("Serving the backend!");
+// })
+
+//when we go to this address this routes run 
+app.use("/api/user",userRoutes);
+app.use("/api/auth",AuthRoutes);
+
+
+
+//server creation
+app.listen(5000,() =>{
+    console.log("Our Api is working BenStokes");
+})

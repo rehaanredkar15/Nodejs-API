d4411a46aa62ed9fbd386295aa81993ca5d6f8c9
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-10-28 17:55:58 +0530

    fixed - fixed the issues of updating user

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,33 @@
 // using Router of express
 const router = require("express").Router();
-const User = require("../Model/User");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 
-
 //REGISTER
-router.post("/register", async(req,res) => {
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-     
-     try{
-         //now to encrypt the password we use  
-          const salt = await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-
-
-   // taking the values from schema and making user to save in the database 
-        const newUser = new User({
-
-            username:req.body.username,
-            email:req.body.email,
-            password:hashedPassword,
-        })
-
-
-        const user = await newUser.save();
-        res.status(200).json(user)
-     }
-     catch(err){
-         res.status(500).json(err);
-     }
-
-    //this operation will be with database to save 
-    // await user.save();   
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
+
+
+
 
 //Login
 router.post("/login", async(req,res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-// using Router of express
-
+const User = require("../models/User");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 //update user
-router.put("/:id",async(req,res) => {
-if (req.body.userId === req.params.id || req.body.isAdmin) {
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -24,7 +24,28 @@ if (req.body.userId === req.params.id || req.body.isAdmin) {
   } else {
     return res.status(403).json("You can update only your account!");
   }
+});
+
+//delete user 
+router.delete("/:id", async(req,res) => {
+
+  if(req.body.userId == req.params.id || req.user.isAdmin)
+  {
+    try{
+      const user = await User.deleteOne(req.params.id);
+      res.status(200).json("Account has been deleted");
+    }
+    catch(err){
+      return res.status(500).json(err);
+
+    }
+
+  }
+  else{
+    return res.status(403).json("You can delete only your account!"); 
+  }
 })
+
 
 module.exports = router;
 

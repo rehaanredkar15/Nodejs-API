554f959acf136029699c4ab8b26be2cad46cdcad
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-10-28 23:58:16 +0530

    feat - added the searching user functionality to the api

--- routes/users.js
+++ routes/users.js
@@ -47,5 +47,22 @@ router.delete("/:id", async(req,res) => {
 })
 
 
+//get user 
+
+router.get("/:id", async(req,res) => {
+
+  try{
+     const user = await User.findById(req.params.id);
+     res.status(200).json(user);
+
+  }
+  catch(err){
+     res.status(500).json(err);
+  }
+
+
+})
+
+
 module.exports = router;
 
